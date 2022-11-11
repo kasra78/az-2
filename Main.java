1a92b8db09d8d7cfe45f5102a9806139e831d8d4
kasra78 <damavandi_kasra@yahoo.com>
2022-11-11 16:37:38 +0330

    Complete main function

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 2 numbers:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("If you want to add the numbers enter 1, if you want to subtract them enter 2:");
+        int x = scanner.nextInt();
+        if (x == 1)
+            System.out.println(plus(a, b));
+        else if (x == 2)
+            System.out.println(subtract(a, b));
+        else
+            System.out.println("Error!");
     }
 
     private static int plusOne(int a) {
